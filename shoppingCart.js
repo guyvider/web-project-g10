4c79c5bba7dfc150c106be78c9c5ed1337cb6ca8
manuelzl <88432992+manuelzl@users.noreply.github.com>
2021-09-08 18:26:20 +0300

    Add files via upload

---
+++ shoppingCart.js
@@ -0,0 +1,8 @@
+// JavaScript code for the shpongcart
+$(document).ready(function () {
+	$(".checkout").on("keyup", ".quantity", function () {
+		var price = +$(".price").data("price");
+		var quantity = +$(this).val();
+		$("#total").text("$" + price * quantity);
+	})
+})
