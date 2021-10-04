5f7bc10b0ae57c2d8ccf5a4abafa42775af5727e
guyvider <88592539+guyvider@users.noreply.github.com>
2021-10-04 22:30:24 +0300

    Delete shoppingcart.js

--- shoppingcart.js
+++
@@ -1,8 +0,0 @@
-// JavaScript code for the shpongcart
-$(document).ready(function () {
-	$(".checkout").on("keyup", ".quantity", function () {
-		var price = +$(".price").data("price");
-		var quantity = +$(this).val();
-		$("#total").text("$" + price * quantity);
-	})
-})
